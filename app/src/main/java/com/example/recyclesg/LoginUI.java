4c1d3456c8f6cee1ec3289174b804952165bb06d
woblek <42810002+woblek@users.noreply.github.com>
2019-07-19 11:06:37 +0800

    Edited Login UI

---
+++ app/src/main/java/com/example/recyclesg/LoginUI.java
@@ -0,0 +1,14 @@
+package com.example.recyclesg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_ui);
+    }
+}
